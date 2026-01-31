440c57dabcf74e5cf69f9fe71aa9f5e9b90400c4
Ghanashyam Vagale <73483236+GhanashyamVagale@users.noreply.github.com>
2026-01-31 00:59:01 -0500

    Final Commit

--- Lab1/src/main.py
+++ Lab1/src/main.py
@@ -1,22 +1,19 @@
-# Import necessary libraries
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 import joblib
 
 if __name__ == '__main__':
-    # Load the Breast Cancer dataset
-    breast = load_breast_cancer()
-    X, y = breast.data, breast.target
+    cancer = load_breast_cancer()
+    X, y = cancer.data, cancer.target
 
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42, stratify=y
+    )
 
-    # Train a Random Forest classifier
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = LogisticRegression(max_iter=2000, solver="liblinear", random_state=42)
     model.fit(X_train, y_train)
 
-    # Save the model to a file
-    joblib.dump(model, 'breast_model.pkl')
-    
+    joblib.dump(model, 'breast_cancer_model.pkl')
+
     print("The model training was successful")
